77e5a8555b672b6c13b1f7948156308f23338a8f
Dhruv Madaan <madaandhruv50@gmail.com>
2024-12-10 21:12:47 +0530

    dsc

--- backend/server.js
+++ backend/server.js
@@ -16,7 +16,12 @@ const PORT = process.env.PORT || 5000;
 app.use(cookieParser())
 app.use(express.json({ limit: '10mb' }));
 app.use(express.urlencoded({ extended: true, limit: '10mb' }));
-app.use(cors());
+app.use(
+    cors({
+        origin: "https://chatt-app-frontend.vercel.app",
+        credentials: true,
+    })
+);
 
 
 app.use('/api/auth', authRoutes)
