fe454520d47c3c8c0061fb558b497375416db218
Dhruv Madaan <madaandhruv50@gmail.com>
2024-12-10 21:21:42 +0530

    asdeff

--- backend/server.js
+++ backend/server.js
@@ -19,6 +19,7 @@ app.use(express.urlencoded({ extended: true, limit: '10mb' }));
 app.use(
     cors({
         origin: "https://chatt-app-frontend.vercel.app",
+        methods: ["GET", "POST"],
         credentials: true,
     })
 );
