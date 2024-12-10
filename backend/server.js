99a0889e4a8addd91dba47709721220afbf88733
Dhruv Madaan <madaandhruv50@gmail.com>
2024-12-10 21:11:13 +0530

    fixing errors

--- backend/server.js
+++ backend/server.js
@@ -16,12 +16,7 @@ const PORT = process.env.PORT || 5000;
 app.use(cookieParser())
 app.use(express.json({ limit: '10mb' }));
 app.use(express.urlencoded({ extended: true, limit: '10mb' }));
-app.use(
-    cors({
-        origin: "https://chatt-app-frontend.vercel.app",
-        credentials: true,
-    })
-);
+app.use(cors());
 
 
 app.use('/api/auth', authRoutes)
