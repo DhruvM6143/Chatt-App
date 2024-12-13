c91396809d352931c9b71aea5a0ab4eb4e3412ab
DhruvM6143 <dhruvmm098@gmail.com>
2024-12-13 17:19:58 +0530

    Update socket.js

--- backend/lib/socket.js
+++ backend/lib/socket.js
@@ -8,7 +8,7 @@ const server = http.createServer(app)
 
 const io = new Server(server, {
     cors: {
-        origin: "https://chatt-app-frontend.vercel.app",
+        origin: "https://chatt-app-backend.vercel.app",
            methods: ["GET", "POST"],
         credentials: true,
     }
