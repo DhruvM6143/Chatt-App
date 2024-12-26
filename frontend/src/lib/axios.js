67bb889a9c254607eeae2c2b386b0d29382afc96
DhruvM6143 <dhruvmm098@gmail.com>
2024-12-26 23:50:03 +0530

    Update axios.js

--- frontend/src/lib/axios.js
+++ frontend/src/lib/axios.js
@@ -1,7 +1,7 @@
 import axios from 'axios'
 
 export const axiosInstance = axios.create({
-    baseURL: 'http://localhost:5000/api',
+    baseURL: 'https://chatt-app-backend-75bw.onrender.com/api',
     withCredentials: true,  // send cookies when making requests
     headers: {
         'Content-Type': 'application/json'
